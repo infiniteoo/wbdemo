33a1ef9ae94d13d6ceb43e65a44fd08177b09839
infiniteoo <troydorman@gmail.com>
2023-11-29 18:41:04 -0700

    added basic crud functions [

--- app.py
+++ app.py
@@ -7,7 +7,30 @@ mongo = PyMongo(app)
 
 @app.route('/items', methods=['GET'])
 def get_items():
+    
     return jsonify({'message': 'Get items endpoint'})
+
+
+@app.route('/items', methods=['POST'])
+def post_item():
+    
+    return jsonify({'message': 'Post item endpoint'})
+
+
+@app.route('/items/<id>', methods=['GET'])
+def get_item(id):
+    
+    return jsonify({'message': 'Get item endpoint'})
+
+@app.route('/items/<id>', methods=['PUT'])
+def put_item(id):
+    
+    return jsonify({'message': 'Put item endpoint'})
+
+@app.route('/items/<id>', methods=['DELETE'])
+def delete_item(id):
+    
+    return jsonify({'message': 'Delete item endpoint'})
 
 
 
