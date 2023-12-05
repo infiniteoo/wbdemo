4c4b2610e742d00de883897e8f56ffc1589e9df6
infiniteoo <troydorman@gmail.com>
2023-12-04 18:14:54 -0700

    refresh icon implemented and route touched

--- client/src/app/image-viewer/image-viewer.component.ts
+++ client/src/app/image-viewer/image-viewer.component.ts
@@ -1,7 +1,8 @@
 // image-viewer.component.ts
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { CharacterDetails } from '../character-list/character-list.component'; // Adjust the path as needed
+import { CharacterDetails } from '../character-list/character-list.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-image-viewer',
@@ -14,4 +15,12 @@ export class ImageViewerComponent {
   @Input() imageUrl?: string;
   @Input() isLoading?: boolean;
   @Input() characterDetails?: CharacterDetails;
+
+  constructor(private http: HttpClient) {}
+
+  refreshImage() {
+    this.http.get('http://localhost:5000/regenerate').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
